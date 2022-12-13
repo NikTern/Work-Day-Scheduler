258004b96efd5f2ca78d3b4321f147efe9509442
Nik <90584544+NikTern@users.noreply.github.com>
2022-12-13 10:27:25 +0800

    fixed colouring bug

--- Develop/script.js
+++ Develop/script.js
@@ -31,58 +31,86 @@ $(function () {
   else{
     var currentAM = false
   }
-  
+
   // Check hour of each div against current hour (every second?)
-  // hrs 9-12am
+  // hrs 9-11am, 12pm
   for (var i=9 ; i < 13; i++){   
     var divAM = true
-  
-    // If both are AM/PM, compare numbers
-    if (currentAM === divAM){
-      if(Number($(`#hour-${i}`).attr('id').substr(5)) < Number(currenthournumber)){
-        (`#hour-${i}`).addClass("past")
-      }
-      else if (Number($(`#hour-${i}`).attr('id').substr(5)) == Number(currenthournumber)){
-        $(`#hour-${i}`).addClass("present")
-      }
-      else if (Number($(`#hour-${i}`).attr('id').substr(5)) > Number(currenthournumber)){
-        $(`#hour-${i}`).addClass("future")
-      }
+
+    if (i == 12){
+      divAM = false
     }
-     //If current time is PM but divtime is AM
-    else if (currentAM == false && divAM == true){
+
+    // FOR i == 12 when the current time is 1-5pm
+    // If both are PM, compare numbers
+    if (currenthournumber==1 || currenthournumber==2 || currenthournumber==3 || currenthournumber==4 || currenthournumber==5){
       $(`#hour-${i}`).addClass("past")
     }
-    else{
-      console.log("something wrong!")
+
+    // FOR i == 9, 10, 11, 12 when the current time is 9-11am or 12pm
+    // If both are AM/PM, compare numbers
+    else if(currenthournumber==9 || currenthournumber==10 || currenthournumber==11 || currenthournumber==12){
+      if (currentAM == divAM){
+        if(Number($(`#hour-${i}`).attr('id').substr(5)) < Number(currenthournumber)){
+          $(`#hour-${i}`).addClass("past")
+        }
+        else if (Number($(`#hour-${i}`).attr('id').substr(5)) == Number(currenthournumber)){
+          $(`#hour-${i}`).addClass("present")
+        }
+        else if (Number($(`#hour-${i}`).attr('id').substr(5)) > Number(currenthournumber)){
+          $(`#hour-${i}`).addClass("future")
+        }
+      }
+
+      //If current time is PM but divtime is AM
+      else if (currentAM == false && divAM == true){
+        $(`#hour-${i}`).addClass("past")
+      }
+      
+      //If current time is AM but divtime is PM
+      else if (currentAM == true && divAM == false){
+        $(`#hour-${i}`).addClass("future")
+      }
+
+      else{
+        console.log("something wrong!")
+      }
     }
   }
 
   //hours 1-5pm
   for (var i=1 ; i < 6; i++){
     var divAM = false
-
-    // If both are AM/PM, compare numbers
-    if (currentAM === divAM){
-      if(Number($(`#hour-${i}`).attr('id').substr(5)) < Number(currenthournumber)){
-        $(`#hour-${i}`).addClass("past")
-      }
-      else if (Number($(`#hour-${i}`).attr('id').substr(5)) == Number(currenthournumber)){
-        $(`#hour-${i}`).addClass("present")
-      }
-      else if (Number($(`#hour-${i}`).attr('id').substr(5)) > Number(currenthournumber)){
-        $(`#hour-${i}`).addClass("future")
-      }
-    }
-    //If current time is AM but divtime is PM
-    else if (currentAM == true && divAM == false){
+    // FOR CURRENT-TIME 12PM: set 1-5PM as 'future'
+    if(currenthournumber==9 || currenthournumber==10 || currenthournumber==11 || currenthournumber==12){
       $(`#hour-${i}`).addClass("future")
     }
-    else{
-      console.log("something wrong here!")
+    // FOR CURRENT-TIMES 1-5PM: compare numbers
+    else if(currenthournumber==1 || currenthournumber==2 || currenthournumber==3 || currenthournumber==4 || currenthournumber==5){
+      if (currentAM == divAM){
+        if(Number($(`#hour-${i}`).attr('id').substr(5)) < Number(currenthournumber)){
+          $(`#hour-${i}`).addClass("past")
+        }
+        else if (Number($(`#hour-${i}`).attr('id').substr(5)) == Number(currenthournumber)){
+          $(`#hour-${i}`).addClass("present")
+        }
+        else if (Number($(`#hour-${i}`).attr('id').substr(5)) > Number(currenthournumber)){
+          $(`#hour-${i}`).addClass("future")
+        }
+      }
+    
+      //If current time is AM but divtime is PM
+      else if (currentAM == true && divAM == false){
+        $(`#hour-${i}`).addClass("future")
+      }
+
+      else{
+        console.log("something wrong")
+      }
     }
   }
   }, 1000)
+
 
   // Code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements.
